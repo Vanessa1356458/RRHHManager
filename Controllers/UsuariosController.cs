2a8cdc6b5f33aa4deba8a180df548af41acc2f20
Vanessa Rodriguez <cynthiavanessa370@gmail.com>
2026-08-31 18:40:38 -0600

    Creacion de Controllers Departamentos y Ususarios

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RRHHManager.Data;
+using RRHHManager.Models;
+
+namespace RRHHManager.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Usuarios.Include(u => u.Empleado).Include(u => u.Rol);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .Include(u => u.Empleado)
+                .Include(u => u.Rol)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id");
+            ViewData["RolId"] = new SelectList(_context.Roles, "Id", "Id");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EmpleadoId,NombreUsuario,Contrasena,RolId")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", usuario.EmpleadoId);
+            ViewData["RolId"] = new SelectList(_context.Roles, "Id", "Id", usuario.RolId);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", usuario.EmpleadoId);
+            ViewData["RolId"] = new SelectList(_context.Roles, "Id", "Id", usuario.RolId);
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,EmpleadoId,NombreUsuario,Contrasena,RolId")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", usuario.EmpleadoId);
+            ViewData["RolId"] = new SelectList(_context.Roles, "Id", "Id", usuario.RolId);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .Include(u => u.Empleado)
+                .Include(u => u.Rol)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.Id == id);
+        }
+    }
+}
